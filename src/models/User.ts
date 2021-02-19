0348bea98fa110845db7753df061e8e0d5183118
Andrew Andersen <andersac88@gmail.com>
2021-02-19 08:04:51 -0600

    begin

---
+++ src/models/User.ts
@@ -0,0 +1,21 @@
+interface UserProps {
+	name?: string;
+	age?: number;
+}
+
+//type alias
+type Callback = () => {};
+
+export class User {
+	constructor(private data: UserProps) {}
+
+	get(propName: string): string | number {
+		return this.data[propName];
+	}
+
+	set(update: UserProps): void {
+		Object.assign(this.data, update);
+	}
+
+	on(eventName: string, callback: Callback) {}
+}
